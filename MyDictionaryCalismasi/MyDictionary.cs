c25ffab4e546b504c158db34e2ecb70906c4edd0
kaderNurTekin <tekinkadernur@gmail.com>
2021-02-03 01:20:22 +0300

    Add project files.

---
+++ MyDictionaryCalismasi/MyDictionary.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryCalismasi
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] Keys;
+        TValue[] Values; //key ve value değerlerini tutmak için iki dizi tanımladık.
+
+        //Constructor
+        public MyDictionary()
+        {
+            Keys = new TKey[0];
+            Values = new TValue[0];
+            //default olarak 0.indexteki değerleri atadık.
+
+        }
+        public void Add(TKey key,TValue value)
+        {
+            // değerleri kaybetmemek için temp değişkenlerinde dizinin önceki kopyası tutulur.
+
+            TKey[] TempKey = Keys;
+            TValue[] TempValue = Values;
+
+            Keys = new TKey[Keys.Length + 1];
+            Values = new TValue[Values.Length + 1];
+
+            //for döngüsüyle templer içerisindeki değerleri şimdiki alana aktaralım.
+
+            for (int i = 0; i < TempKey.Length;i++)
+            {
+                Keys[i] = TempKey[i];
+                Values[i] = TempValue[i];
+            }
+
+            //şimdide son değerride ekleyelim.
+            Keys[Keys.Length - 1] = key;
+            Values[Values.Length - 1] = value;
+
+
+        }
+
+        public int Length
+        {
+            get { return Keys.Length; }
+        }
+        public TKey[]keys
+        {
+            get { return Keys; }
+        }
+        public TValue [] values
+        {
+            get { return Values; }
+        }
+
+        //Liste elemanlarını listeleyen metod.
+        public void List()
+        {
+            for(int i=0;i<Keys.Length;i++)
+            {
+                Console.WriteLine(Keys[i]+" " +Values[i]);
+            }
+        }
+    }
+}
---
+++ MyDictionaryCalismasi/Program.cs
@@ -0,0 +1,19 @@
+﻿ using System;
+
+namespace MyDictionaryCalismasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> esyalar = new MyDictionary<int, string>();
+
+            esyalar.Add(1, "Lamba");
+            esyalar.Add(2, "Koltuk Takımı");
+            esyalar.Add(3, "Televizyon");
+            esyalar.Add(4, "Halı");
+            esyalar.List();
+
+        }
+    }
+}
